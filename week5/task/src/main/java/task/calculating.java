7c2c790cdc683067eecbe7f99efe66cd93d97a85
170031410 <59142850+170031410@users.noreply.github.com>
2020-03-04 23:00:33 +0530

    Add files via upload

---
+++ week5/task/src/main/java/task/calculating.java
@@ -0,0 +1,9 @@
+package task;
+
+public class cluclating{
+	Double calculation(Double amount,Double rate,Double duration)
+	{
+		Double simpleintrest=(amount*rate*duration)/100;
+		return simpleintrest;
+	}
+}
